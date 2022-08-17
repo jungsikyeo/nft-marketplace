7397623ac2515ac012c51043e0810c2633c6cd60
jungsik.yeo <jungsik.yeo@sk.com>
2022-08-18 01:42:21 +0900

    Section 12 complete

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721 {}
+contract ERC721 {
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 indexed tokenId
+  );
+
+  mapping(uint256 => address) private _tokenOwner;
+  mapping(address => uint256) private _OwnedTokensCount;
+
+  function _exist(uint256 tokenId) internal view returns (bool) {
+    address owner = _tokenOwner[tokenId];
+    return owner != address(0);
+  }
+
+  function _mint(address to, uint256 tokenId) internal {
+    require(to != address(0), "ERC721: minting to the zero address");
+    require(!_exist(tokenId), "ERC721: token already minted");
+    _tokenOwner[tokenId] = to;
+    _OwnedTokensCount[to] += 1;
+
+    emit Transfer(address(0), to, tokenId);
+  }
+}
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,5 +4,23 @@ pragma solidity ^0.8.0;
 import "./ERC721Connector.sol";
 
 contract Kryptobird is ERC721Connector {
+  string[] public KryptoBirdz;
+
+  mapping(string => bool) _kryptoBirdzExists;
+
+  function mint(string memory _kryptoBird) public {
+    require(
+      !_kryptoBirdzExists[_kryptoBird],
+      "Error - kryptoBird already exists"
+    );
+
+    KryptoBirdz.push(_kryptoBird);
+    uint256 _id = KryptoBirdz.length - 1;
+
+    _mint(msg.sender, _id);
+
+    _kryptoBirdzExists[_kryptoBird] = true;
+  }
+
   constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
 }
