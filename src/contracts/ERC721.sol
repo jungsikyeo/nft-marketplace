85a5d80dca05070ca7fd6ad16d86b2fd394a8b67
jungsik.yeo <jungsik.yeo@sk.com>
2022-08-18 15:55:55 +0900

    Section 13 complete

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -11,12 +11,23 @@ contract ERC721 {
   mapping(uint256 => address) private _tokenOwner;
   mapping(address => uint256) private _OwnedTokensCount;
 
+  function balanceOf(address _owner) public view returns (uint256) {
+    require(_owner != address(0), "owner query for non-existent token");
+    return _OwnedTokensCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    address owner = _tokenOwner[_tokenId];
+    require(owner != address(0), "owner query for non-existent token");
+    return owner;
+  }
+
   function _exist(uint256 tokenId) internal view returns (bool) {
     address owner = _tokenOwner[tokenId];
     return owner != address(0);
   }
 
-  function _mint(address to, uint256 tokenId) internal {
+  function _mint(address to, uint256 tokenId) internal virtual {
     require(to != address(0), "ERC721: minting to the zero address");
     require(!_exist(tokenId), "ERC721: token already minted");
     _tokenOwner[tokenId] = to;
